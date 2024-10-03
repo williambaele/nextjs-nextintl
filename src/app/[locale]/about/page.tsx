e2c348b40a1f0f53787fca196396e74d4e4df56a
William Baele <baelewilliam1999@gmail.com>
2024-10-03 14:30:21 +0200

    Added 404 page

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,7 +1,6 @@
 import { useTranslations } from 'next-intl';
 import Navigation from '@/components/Navigation';
 export default function Example() {
-  const h = useTranslations('Navigation');
   const t = useTranslations('AboutPage');
 
 
