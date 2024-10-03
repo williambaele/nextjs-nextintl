58703cccd880ca52bddd5143384492e54579e39e
William Baele <baelewilliam1999@gmail.com>
2024-10-03 14:16:26 +0200

    Added 404 page

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,5 +1,5 @@
 import { useTranslations } from 'next-intl';
-
+import Navigation from '@/components/Navigation';
 export default function Example() {
   const h = useTranslations('Navigation');
   const t = useTranslations('AboutPage');
@@ -7,6 +7,7 @@ export default function Example() {
 
   return (
     <>
+    <Navigation/>
       <div className='root-container'>
         <div className='custom-container'>
           <h1 className='h1-custom'>{t('Title')}</h1>
