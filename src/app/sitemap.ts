4f48cc9be9f823c94cb49070f1f5bb7518ceedde
William Baele <baelewilliam1999@gmail.com>
2024-10-03 14:10:49 +0200

    Added 404 page

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,7 +3,7 @@ import {host} from '@/config';
 import {Locale, getPathname, routing} from '@/i18n/routing';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  return [getEntry('/'), getEntry('/pathnames')];
+  return [getEntry('/'), getEntry('/about')];
 }
 
 type Href = Parameters<typeof getPathname>[0]['href'];
